6630abe0e6dab1ce5682241d43c2c285582b9012
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 21:14:49 +0530

    updated DriverFactory with firefox driver

--- src/main/java/com/bagal/factories/DriverFactory.java
+++ src/main/java/com/bagal/factories/DriverFactory.java
@@ -2,6 +2,7 @@ package com.bagal.factories;
 
 import com.bagal.exceptions.InvalidBrowserNameException;
 import com.bagal.factories.browseroptions.ChromeBrowserOptions;
+import com.bagal.factories.browseroptions.FirefoxBrowserOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -15,7 +16,7 @@ public final class DriverFactory {
         if(getTestFireConfigs().browser().equalsIgnoreCase("chrome")){
             return new ChromeDriver(ChromeBrowserOptions.getChromeOptions());
         } else if (getTestFireConfigs().browser().equalsIgnoreCase("firefox")) {
-            return new FirefoxDriver();
+            return new FirefoxDriver(FirefoxBrowserOptions.getFirefoxOptions());
         } else {
             throw new InvalidBrowserNameException(getTestFireConfigs().browser()+"Invalid Browser Name ");
         }
