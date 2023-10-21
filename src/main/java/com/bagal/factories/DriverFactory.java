305efcd0312e726c4021f90e369a9e38d002022f
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 13:22:38 +0530

    created DriverFactory class for creation of drivers

--- src/main/java/com/bagal/factories/DriverFactory.java
+++ src/main/java/com/bagal/factories/DriverFactory.java
@@ -1,4 +1,23 @@
 package com.bagal.factories;
 
-public class DriverFactory {
+import com.bagal.exceptions.InvalidBrowserNameException;
+import com.bagal.factories.browseroptions.ChromeBrowserOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static com.bagal.configs.TestFireConfigReader.getTestFireConfigs;
+
+public final class DriverFactory {
+    private DriverFactory(){}
+    public static WebDriver getDriver(){
+
+        if(getTestFireConfigs().browser().equalsIgnoreCase("chrome")){
+            return new ChromeDriver(ChromeBrowserOptions.getChromeOptions());
+        } else if (getTestFireConfigs().browser().equalsIgnoreCase("firefox")) {
+            return new FirefoxDriver();
+        } else {
+            throw new InvalidBrowserNameException(getTestFireConfigs().browser()+"Invalid Browser Name ");
+        }
+    }
 }
