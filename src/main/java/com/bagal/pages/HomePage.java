711ef798b03164fd72a0152d858d44f1e8df3d28
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 22:17:44 +0530

    added abstract method

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -1,6 +1,40 @@
 package com.bagal.pages;
 
 import com.bagal.pages.basepage.BasePage;
+import org.openqa.selenium.By;
+
+import static com.bagal.uiactions.PageAction.click;
+import static com.bagal.uiactions.PageAction.isDisplayed;
 
 public class HomePage extends BasePage {
+    private By singInLink = By.id("AccountLink");
+
+    @Override
+    public boolean isPageLoaded() {
+        return isDisplayed(singInLink);
+    }
+
+    public HomePage clickOnSignInLink() {
+        click(singInLink);
+        return this;
+    }
+
+    public void loginToApplication(String username, String password) {
+        enterUserName(username)
+                .enterPassword(password)
+                .clickOnLogin();
+    }
+
+    private HomePage enterUserName(String username) {
+        return this;
+    }
+
+    private HomePage enterPassword(String password) {
+        return this;
+    }
+
+    private HomePage clickOnLogin() {
+        return this;
+    }
+
 }
