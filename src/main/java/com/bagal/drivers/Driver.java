827c4532a7b97914181804b6354bd228714af5b8
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 13:21:15 +0530

    updated driver with driver code

--- src/main/java/com/bagal/drivers/Driver.java
+++ src/main/java/com/bagal/drivers/Driver.java
@@ -1,4 +1,23 @@
 package com.bagal.drivers;
 
-public class Driver {
+import com.bagal.factories.DriverFactory;
+
+import java.util.Objects;
+
+import static com.bagal.configs.TestFireConfigReader.getTestFireConfigs;
+
+public final class Driver {
+    private Driver(){}
+    public static void initDriver(){
+        if(Objects.isNull(DriverManager.getDriver())){
+            DriverManager.setDriver(DriverFactory.getDriver());
+            DriverManager.getDriver().get(getTestFireConfigs().url());
+        }
+    }
+    public static void quitDriver(){
+        if(Objects.nonNull(DriverManager.getDriver())){
+            DriverManager.getDriver().quit();
+            DriverManager.unload();
+        }
+    }
 }
