0ca099b977a77e964c924ca8d78d03523da8943b
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 13:08:36 +0530

    created ChromeBrowserOptions for chrome options

---
+++ src/main/java/com/bagal/factories/browseroptions/ChromeBrowserOptions.java
@@ -0,0 +1,15 @@
+package com.bagal.factories.browseroptions;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public final class ChromeBrowserOptions {
+    private ChromeBrowserOptions(){}
+
+    public static ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        return options;
+    }
+}
