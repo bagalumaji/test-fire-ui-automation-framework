4d5dd1d7796a2a2b54f5c66c8be7bbf882b72039
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 13:22:10 +0530

    updated test base class with driver init and quit method

--- src/test/java/com/bagal/testbase/TestBase.java
+++ src/test/java/com/bagal/testbase/TestBase.java
@@ -1,24 +1,17 @@
 package com.bagal.testbase;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import com.bagal.drivers.Driver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import static com.bagal.configs.TestFireConfigReader.getTestFireConfigs;
-
 public class TestBase {
-    protected WebDriver driver;
-
     @BeforeMethod
     public void setup() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(getTestFireConfigs().url());
+        Driver.initDriver();
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        Driver.quitDriver();
     }
 }
