29378902194568b4989ace08dda0d500ae731b3b
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 13:21:41 +0530

    updated driver manager class with thread local

--- src/main/java/com/bagal/drivers/DriverManager.java
+++ src/main/java/com/bagal/drivers/DriverManager.java
@@ -1,4 +1,24 @@
 package com.bagal.drivers;
 
-public class DriverManager {
+import org.openqa.selenium.WebDriver;
+
+import java.util.Objects;
+
+public final class DriverManager {
+    private DriverManager(){}
+    private static final ThreadLocal<WebDriver> WEB_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+
+    static void setDriver(WebDriver driver){
+        if(Objects.nonNull(driver)){
+            WEB_DRIVER_THREAD_LOCAL.set(driver);
+        }
+    }
+
+    public static WebDriver getDriver(){
+        return WEB_DRIVER_THREAD_LOCAL.get();
+    }
+
+    public static void unload(){
+        WEB_DRIVER_THREAD_LOCAL.remove();
+    }
 }
