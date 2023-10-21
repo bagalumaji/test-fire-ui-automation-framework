ee340b8ec49970e259fcf7edddfca67d0c977a53
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 21:16:19 +0530

    created firefox browser option

---
+++ src/main/java/com/bagal/factories/browseroptions/FirefoxBrowserOptions.java
@@ -0,0 +1,18 @@
+package com.bagal.factories.browseroptions;
+
+import com.bagal.configs.TestFireConfigReader;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public final class FirefoxBrowserOptions {
+    private FirefoxBrowserOptions(){}
+
+    public static FirefoxOptions getFirefoxOptions(){
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--private");
+        if(TestFireConfigReader.getTestFireConfigs().headless()){
+            options.addArguments("--headless");
+        }
+        return options;
+    }
+}
