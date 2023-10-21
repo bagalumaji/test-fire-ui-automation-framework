9387f7e354aac96812ee77ed315c6f709961b918
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-21 12:32:11 +0530

    read app prop in test

--- src/test/java/com/bagal/tests/LoginTests.java
+++ src/test/java/com/bagal/tests/LoginTests.java
@@ -1,11 +1,11 @@
 package com.bagal.tests;
 
+import com.bagal.configs.TestFireConfigReader;
 import org.testng.annotations.Test;
 
 public class LoginTests {
     @Test
     public void loginTest(){
-
-
+        System.out.println("ConfigReader.getTestFireConfigs().url() = " + TestFireConfigReader.getTestFireConfigs().url());
     }
 }
