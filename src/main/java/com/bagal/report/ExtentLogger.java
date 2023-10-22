7eb4b8bd11167722ccde0a3384d551187cc7fbbe
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-22 22:57:15 +0530

    added info, pass, fail, and skip method

--- src/main/java/com/bagal/report/ExtentLogger.java
+++ src/main/java/com/bagal/report/ExtentLogger.java
@@ -2,4 +2,16 @@ package com.bagal.report;
 
 public final class ExtentLogger {
     private ExtentLogger(){}
+    public static void info(String message){
+        ExtentManager.getExtentTest().info(message);
+    }
+    public static void pass(String message){
+        ExtentManager.getExtentTest().pass(message);
+    }
+    public static void fail(String message){
+        ExtentManager.getExtentTest().fail(message);
+    }
+    public static void skip(String message){
+        ExtentManager.getExtentTest().skip(message);
+    }
 }
