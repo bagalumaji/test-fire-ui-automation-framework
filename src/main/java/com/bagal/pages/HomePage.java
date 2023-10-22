7fe0c8a37e9f3adfcfaa53c90147d3b0febfee89
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-22 22:37:27 +0530

    updated home page with locators and methods

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -3,19 +3,22 @@ package com.bagal.pages;
 import com.bagal.pages.basepage.BasePage;
 import org.openqa.selenium.By;
 
-import static com.bagal.uiactions.PageAction.click;
-import static com.bagal.uiactions.PageAction.isDisplayed;
+import static com.bagal.uiactions.PageAction.*;
 
 public class HomePage extends BasePage {
-    private By singInLink = By.id("AccountLink");
+    private final static By SING_IN_LINK = By.id("AccountLink");
+    private final static By TEXT_BOX_USER_NAME = By.id("uid");
+    private final static By TEXT_BOX_PASSWORD = By.id("passw");
+    private final static By BTN_LOGIN = By.name("btnSubmit");
+
 
     @Override
     public boolean isPageLoaded() {
-        return isDisplayed(singInLink);
+        return isDisplayed(SING_IN_LINK);
     }
 
     public HomePage clickOnSignInLink() {
-        click(singInLink);
+        click(SING_IN_LINK);
         return this;
     }
 
@@ -26,14 +29,17 @@ public class HomePage extends BasePage {
     }
 
     private HomePage enterUserName(String username) {
+        type(TEXT_BOX_USER_NAME,username);
         return this;
     }
 
     private HomePage enterPassword(String password) {
+        type(TEXT_BOX_PASSWORD,password);
         return this;
     }
 
     private HomePage clickOnLogin() {
+        click(BTN_LOGIN);
         return this;
     }
 
