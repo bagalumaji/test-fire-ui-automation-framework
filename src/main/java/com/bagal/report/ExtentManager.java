aac5b59fe51ea1cc4ded6f2b20c763b19ad26844
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-22 22:58:00 +0530

    added thread local to set and get extent test

--- src/main/java/com/bagal/report/ExtentManager.java
+++ src/main/java/com/bagal/report/ExtentManager.java
@@ -1,4 +1,25 @@
 package com.bagal.report;
 
-public class ExtentManager {
+import com.aventstack.extentreports.ExtentTest;
+
+import java.util.Objects;
+
+public final class ExtentManager {
+    private ExtentManager(){}
+
+    private static final ThreadLocal<ExtentTest> EXTENT_TEST_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static ExtentTest getExtentTest(){
+        return EXTENT_TEST_THREAD_LOCAL.get();
+    }
+
+    static void setExtentTest(ExtentTest test){
+        if(Objects.nonNull(test)){
+            EXTENT_TEST_THREAD_LOCAL.set(test);
+        }
+    }
+
+    public static void unload(){
+        EXTENT_TEST_THREAD_LOCAL.remove();
+    }
 }
