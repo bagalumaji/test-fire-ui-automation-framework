34711fa22b286efa511062e685cab862859843d7
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-22 23:12:55 +0530

    implemented isuite listener

--- src/main/java/com/bagal/listeners/TestFireListener.java
+++ src/main/java/com/bagal/listeners/TestFireListener.java
@@ -1,4 +1,17 @@
 package com.bagal.listeners;
 
-public class TestFireListener {
+import com.bagal.report.ExtentReport;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+public class TestFireListener implements ISuiteListener{
+    @Override
+    public void onStart(ISuite suite) {
+        ExtentReport.initReport();
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+       ExtentReport.flushReport();
+    }
 }
