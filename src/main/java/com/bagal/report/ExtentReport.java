f96cb91ed1d766e0d69022d592a31797fd1b7864
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-22 22:52:16 +0530

    added initReport, createTest, and flushReport Methods

--- src/main/java/com/bagal/report/ExtentReport.java
+++ src/main/java/com/bagal/report/ExtentReport.java
@@ -1,4 +1,34 @@
 package com.bagal.report;
 
-public class ExtentReport {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.bagal.constants.TestFireConstants;
+
+import java.util.Objects;
+
+public final class ExtentReport {
+    private ExtentReport(){}
+    private static ExtentReports extentReports;
+
+    public static void initReport(){
+        if(Objects.isNull(extentReports)){
+            extentReports = new ExtentReports();
+            ExtentSparkReporter sparkReporter = new ExtentSparkReporter(TestFireConstants.getExtentReportFilePath());
+            sparkReporter.config().setReportName(TestFireConstants.getExtentReportName());
+            sparkReporter.config().setDocumentTitle(TestFireConstants.getDocumentTitle());
+            sparkReporter.config().setTheme(Theme.DARK);
+            extentReports.attachReporter(sparkReporter);
+        }
+    }
+
+    public static void createTest(String testCaseName){
+         extentReports.createTest(testCaseName);
+    }
+    public static void flushReport(){
+        if(Objects.nonNull(extentReports)){
+            extentReports.flush();
+        }
+    }
 }
