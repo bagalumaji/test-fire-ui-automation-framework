4b8654e65767507d20bd1e7cebd3d57f85bf3fcf
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-23 16:42:58 +0530

    added extent logger methods pass,info, fail

--- src/main/java/com/bagal/listeners/TestFireListener.java
+++ src/main/java/com/bagal/listeners/TestFireListener.java
@@ -1,10 +1,17 @@
 package com.bagal.listeners;
 
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.bagal.report.ExtentReport;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
 
-public class TestFireListener implements ISuiteListener{
+import static com.bagal.report.ExtentLogger.*;
+
+public class TestFireListener implements ISuiteListener, ITestListener {
     @Override
     public void onStart(ISuite suite) {
         ExtentReport.initReport();
@@ -13,5 +20,27 @@ public class TestFireListener implements ISuiteListener{
     @Override
     public void onFinish(ISuite suite) {
        ExtentReport.flushReport();
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+       ExtentReport.createTest(result.getMethod().getConstructorOrMethod().getMethod().getDeclaredAnnotation(Test.class).description());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        pass( MarkupHelper.createLabel(result.getMethod().getConstructorOrMethod().getMethod().getDeclaredAnnotation(Test.class).description()+ " is passed",ExtentColor.GREEN).getMarkup());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+     //   fail(MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenshotUtil.getScreenshotAsBase64Format()).build().toString());
+        fail(result.getThrowable().getCause().getMessage());
+        fail(MarkupHelper.createLabel(result.getMethod().getConstructorOrMethod().getMethod().getDeclaredAnnotation(Test.class).description()+ " is failed", ExtentColor.RED).getMarkup());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        skip(MarkupHelper.createLabel(result.getMethod().getConstructorOrMethod().getMethod().getDeclaredAnnotation(Test.class).description()+ " is skipped",ExtentColor.YELLOW).getMarkup());
     }
 }
