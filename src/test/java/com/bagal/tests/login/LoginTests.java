b69794b8e4eceeb636c61c725dedc56a119caa45
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-23 16:43:22 +0530

    added description in test annotations

--- src/test/java/com/bagal/tests/login/LoginTests.java
+++ src/test/java/com/bagal/tests/login/LoginTests.java
@@ -1,29 +1,23 @@
 package com.bagal.tests.login;
 
 import com.bagal.pages.HomePage;
-import com.bagal.report.ExtentLogger;
-import com.bagal.report.ExtentReport;
 import com.bagal.testbase.TestBase;
-import org.openqa.selenium.support.pagefactory.ElementLocator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.bagal.configs.TestFireConfigReader.getTestFireConfigs;
 import static com.bagal.report.ExtentLogger.info;
-import static com.bagal.report.ExtentLogger.pass;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class LoginTests extends TestBase {
-    @Test
+    @Test(description = "verify user is able to login to application")
     public void loginTest(){
-        ExtentReport.initReport();
-        ExtentReport.createTest("Login Test");
         HomePage homePage = new HomePage().clickOnSignInLink();
         info("clicked on login link");
         Assert.assertTrue(homePage.isPageLoaded());
         info("login page loaded");
         homePage.loginToApplication(getTestFireConfigs().username(), getTestFireConfigs().password());
         info("username and password entered");
-        pass("login test passed");
-        ExtentReport.flushReport();
+        assertTrue(false);
     }
 }
