5bc2121b8ce3ccfff3b92f29c58e83cddd963334
bagalumaji <bagalumaji@gmail.com>
2023-12-19 12:57:54 +0530

    updated report path to target

--- src/main/java/com/bagal/constants/TestFireConstants.java
+++ src/main/java/com/bagal/constants/TestFireConstants.java
@@ -6,7 +6,7 @@ public final class TestFireConstants {
     private TestFireConstants(){}
 
     private static final String BASE_PATH=System.getProperty("user.dir");
-    private static final @Getter String extentReportFilePath = BASE_PATH+"/reports/index.html";
+    private static final @Getter String extentReportFilePath = BASE_PATH+"target/reports/index.html";
     private static final @Getter String extentReportName="Automation Regression Test Report";
     private static final @Getter String documentTitle = "Automation Test Report - Umaji Bagal";
 }
