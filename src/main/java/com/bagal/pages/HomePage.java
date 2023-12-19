e2f8a8b0286c8319a9ac1198ded4e494fb908589
bagalumaji <bagalumaji@gmail.com>
2023-12-19 12:55:25 +0530

    added ispageloaded method

--- src/main/java/com/bagal/pages/HomePage.java
+++ src/main/java/com/bagal/pages/HomePage.java
@@ -1,46 +1,13 @@
 package com.bagal.pages;
 
-import com.bagal.pages.basepage.BasePage;
+import com.bagal.uiactions.PageAction;
 import org.openqa.selenium.By;
 
-import static com.bagal.uiactions.PageAction.*;
-
-public class HomePage extends BasePage {
-    private final static By SING_IN_LINK = By.id("AccountLink");
-    private final static By TEXT_BOX_USER_NAME = By.id("uid");
-    private final static By TEXT_BOX_PASSWORD = By.id("passw");
-    private final static By BTN_LOGIN = By.name("btnSubmit");
-
-
+public class HomePage extends BasePage{
+    private static final By SIGN_OFF_LINK=By.linkText("Sign Off");
     @Override
     public boolean isPageLoaded() {
-        return isDisplayed(SING_IN_LINK);
-    }
-
-    public HomePage clickOnSignInLink() {
-        click(SING_IN_LINK);
-        return this;
-    }
-
-    public void loginToApplication(String username, String password) {
-        enterUserName(username)
-                .enterPassword(password)
-                .clickOnLogin();
-    }
-
-    private HomePage enterUserName(String username) {
-        type(TEXT_BOX_USER_NAME,username);
-        return this;
-    }
-
-    private HomePage enterPassword(String password) {
-        type(TEXT_BOX_PASSWORD,password);
-        return this;
-    }
-
-    private HomePage clickOnLogin() {
-        click(BTN_LOGIN);
-        return this;
+        return PageAction.isDisplayed(SIGN_OFF_LINK);
     }
 
 }
